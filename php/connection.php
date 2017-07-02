3b3d90511c88d5038512021f7bf0d6924633e8eb
ig4ever <rahmatig4ever@gmail.com>
2017-07-02 17:43:20 +0700

    tes azure connection

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,5 @@
 <?php
-    $host = "localhost";
+    /*$host = "localhost";
     $user = "root";
     $pass = "";
     $database = "db_lowker";
@@ -7,5 +7,21 @@
     $connection = mysqli_connect($host, $user, $pass, $database);
     if (mysqli_connect_errno()) {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }*/
+
+    //Azure Connection
+    // PHP Data Objects(PDO) Sample Code:
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:lowker.database.windows.net,1433; Database = lowker", "admin1", "{your_password_here}");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
+
+    // SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "admin1@lowker", "pwd" => "{Admin4241622}", "Database" => "lowker", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:lowker.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
