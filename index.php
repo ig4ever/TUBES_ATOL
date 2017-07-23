4ffa9b61d61ee01497bf653b81c6d25bdfc7504b
ig4ever <rahmatig4ever@gmail.com>
2017-07-24 05:28:56 +0700

    change header

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <title>Lowker</title>
+        <title>ATOL</title>
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <meta name="viewport" content="width=device-width" />
         <link href="css/bootstrap.min.css" rel="stylesheet" />
