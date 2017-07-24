686794e404c3e8857dd8e0e7dd8607666025e4ee
ig4ever <rahmatig4ever@gmail.com>
2017-07-24 09:30:27 +0700

    tes 5

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <title>ATOL TUBES</title>
+        <title>ATOL</title>
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <meta name="viewport" content="width=device-width" />
-        <!--<link href="css/bootstrap.min.css" rel="stylesheet" />-->
-        <!--<link href="css/index.css" rel="stylesheet" />-->
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/index.css" rel="stylesheet" />
         <!--     Fonts and icons     -->
-        <!--<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,600,700,800,900' rel='stylesheet' type='text/css'>-->
-        <!--<link href="font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">-->
+        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,600,700,800,900' rel='stylesheet' type='text/css'>
+        <link href="font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
         <style>
             .map-info-window{
                 background:#333;
