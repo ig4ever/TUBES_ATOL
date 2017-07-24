10eec6733f1c9f23d035cf00b02eaa901cf23fe0
ig4ever <rahmatig4ever@gmail.com>
2017-07-24 09:23:56 +0700

    tes 1

--- index.php
+++ index.php
@@ -187,7 +187,7 @@
             </div>
         </div>
         <script src="js/jquery.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <!--<script src="js/bootstrap.min.js" type="text/javascript"></script>-->
     </body>
 
     </html>
